0b3434db9b5830374fee3c7aeddc7b4d62b17af8
Ananya Gupta <anannya.gta@gmail.com>
2024-04-10 21:31:09 +0530

    Add react-router-dom to project; update projects data with demo and code links; add styles for project components
    This commit adds react-router-dom to the project and updates the projects data to include demo and code links for each project. It also adds styles for the project components, including the work container, card, image, title, and buttons.

--- src/components/Projects/Data.js
+++ src/components/Projects/Data.js
@@ -10,30 +10,42 @@ export const projectsData = [
     id: 1,
     image: Work1,
     title: "Fitness Mantra",
+    demo: "",
+    code: "https://github.com/ananya-gta/Fitness-Mantra"
   },
   {
     id: 2,
     image: Work2,
-    title: "Ananya's Online Library",
+    title: "Online Library",
+    demo: "",
+    code: "https://github.com/ananya-gta/Library-Management-System-Frontend"
   },
   {
     id: 3,
     image: Work3,
     title: "Note-Me",
+    demo: "https://ananya-noteme.netlify.app/",
+    code: "https://github.com/ananya-gta/note-me"
   },
   {
     id: 4,
     image: Work4,
     title: "SocioBuzz",
+    demo: "https://sociobuzz.netlify.app/",
+    code: "https://github.com/ananya-gta/sociobuzz"
   },
   {
     id: 5,
     image: Work5,
     title: "ImaginAI",
+    demo: "https://imaginai.ananya-gta.repl.co/",
+    code: "https://github.com/ananya-gta/imgaginai"
   },
   {
     id: 6,
     image: Work6,
     title: "Cafeteria Website",
+    demo: "",
+    code: "https://github.com/fitness-mantra"
   },
 ];
--- src/components/Projects/WorkItems.js
+++ src/components/Projects/WorkItems.js
@@ -3,7 +3,16 @@ import React from "react";
 const WorkItems = ({ item }) => {
   return (
     <div className="work__card" key={item.id}>
-      <img src={item.image} alt="" className="work__img"/>
+      <img src={item.image} alt="" className="work__img" />
+      <div className="work__title">
+        <h3 >{item.title}</h3>{" "}
+        <a href={item.demo} className="work__button">
+          <i className="bx bx-link-external work__button-icon"></i>
+        </a>
+      </div>
+      <a href={item.code} className="work__button">
+        Code <i className="bx bx-code-alt work__button-icon"></i>
+      </a>
     </div>
   );
 };
